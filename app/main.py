012c01dc47a6ed37f5a635d36c64db04c6aab604
Krishna7dx9 <sharmakriss985@gmail.com>
2026-07-04 09:17:54 +0000

    Add multi-food nutrition aggregation and total nutrition calculation

--- app/main.py
+++ app/main.py
@@ -45,10 +45,34 @@ async def upload_image(file: UploadFile = File(...)):
             if "error" not in nutrition_data:
                 nutrition_results.append(nutrition_data)
 
+        total_calories = 0
+        total_protein = 0
+        total_fat = 0
+        total_carbs = 0
+        total_sugar = 0
+        total_fiber = 0
+
+        for food in nutrition_results:
+            total_calories += food.get("calories", 0)
+            total_protein += food.get("protein", 0)
+            total_fat += food.get("fat", 0)
+            total_carbs += food.get("carbs", 0)
+            total_sugar += food.get("sugar", 0)
+            total_fiber += food.get("fiber", 0)
+
         return {
             "foods": nutrition_results,
             "total_detected": len(food_list),
-            "total_found": len(nutrition_results)
+            "total_found": len(nutrition_results),
+
+            "total_nutrition": {
+                "calories": round(total_calories, 2),
+                "protein": round(total_protein, 2),
+                "fat": round(total_fat, 2),
+                "carbs": round(total_carbs, 2),
+                "sugar": round(total_sugar, 2),
+                "fiber": round(total_fiber, 2)
+            }
         }
 
     except Exception as exc:
